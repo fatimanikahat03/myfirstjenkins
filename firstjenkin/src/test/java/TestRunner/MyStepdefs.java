ca73fe284142c022ae73bd4ec0897c746efa0855
fatimanikahat03 <133115566+fatimanikahat03@users.noreply.github.com>
2023-08-26 22:11:24 -0400

    Update MyStepdefs.java

--- firstjenkin/src/test/java/TestRunner/MyStepdefs.java
+++ firstjenkin/src/test/java/TestRunner/MyStepdefs.java
@@ -25,7 +25,9 @@ public class MyStepdefs {
 
     @Given("Enter URL in chrome Browser")
     public void enterURLInChromeBrowser() throws IOException, InterruptedException {
-//        driver = BrowserCall.Browserinvocation(); //facebook.com  // implicit timeout enable for execution or sleep method
+
+       System.setProperty("webdriver.edge.driver", "C:\Users\Lenovo\Desktop\edgedriver_win64 (2)");
+      //        driver = BrowserCall.Browserinvocation(); //facebook.com  // implicit timeout enable for execution or sleep method
 EdgeOptions edgeOptions=new EdgeOptions();
   //      edgeOptions.addArguments("--no-sandbox");
     //    edgeOptions.addArguments("--disable-dev-shm-usage");
